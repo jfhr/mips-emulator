ca2eb4a49044defba9343645855752fcbc8da98c
jfhr <31999724+jfhr@users.noreply.github.com>
2020-03-28 01:43:50 +0100

    Add Mips.Assembler Project, AsmInstructions

---
+++ Mips.Assembler/AsmInstructionAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mips.Assembler
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+    sealed class AsmInstructionAttribute : Attribute
+    {
+        public string Pattern { get; }
+
+        public AsmInstructionAttribute(string pattern) => Pattern = pattern ?? throw new ArgumentNullException(nameof(pattern));
+    }
+}
---
+++ Mips.Assembler/Instructions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace Mips.Assembler
+{
+    static class Instructions
+    {
+        // We don't enforce .text and .data
+        // but allow them for compatibility
+        [AsmInstruction(".text")]
+        [AsmInstruction(".data")]
+        public static void Nop() { }
+
+        [AsmInstruction("\\.ascii +\"(?<text>(?>\\\"|[^\"])*)\"")]
+        public static byte[] Ascii(string text)
+        {
+            return Encoding.ASCII.GetBytes(text);
+        }
+
+        [AsmInstruction("\\.asciiz +\"(?<text>(?>\\\"|[^\"])*)\"")]
+        public static byte[] AsciiZ(string text)
+        {
+            int length = Encoding.ASCII.GetByteCount(text) + 1;
+            var bytes = new byte[length];
+            Encoding.ASCII.GetBytes(text, 0, text.Length, bytes, 0);
+            return bytes;
+        }
+
+        [AsmInstruction("\\.byte (?<value>-?(?>0x[0-9a-fA-F]+)|-?(?>[0-9]+)|-?(?>0b[01]+))")]
+        public static byte Byte(string value)
+        {
+            var signedValue = sbyte.Parse(value);
+            var unsignedValue = (byte)signedValue;
+            return unsignedValue;
+        }
+    }
+}
